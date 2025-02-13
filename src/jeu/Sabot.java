28bb391afbb517be48274d01b73ef41d630a0621
quentin_heude <que.heude@gmail.com>
2025-02-13 09:45:55 +0100

    début sabot 2

--- src/jeu/Sabot.java
+++ src/jeu/Sabot.java
@@ -1,5 +1,17 @@
 package jeu;
 
-public class Sabot {
+import cartes.Carte;
 
+public class Sabot {
+	private Carte[] cartes;
+	private int nbCartes;
+	
+	public Sabot(Carte[] cartes, int nbCartes) {
+		this.cartes = cartes;
+		this.nbCartes = nbCartes;
+	}
+	
+	public boolean estVide() {
+		return nbCartes == 0;
+	}
 }
