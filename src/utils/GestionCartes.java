5da1722b20732ce9fd71e21169c930da4d2ae5f1
quentin_heude <que.heude@gmail.com>
2025-04-03 09:45:11 +0200

    quasi lafin du tp 2 plus que verifierrassemblement et q4

--- src/utils/GestionCartes.java
+++ src/utils/GestionCartes.java
@@ -9,59 +9,84 @@ import java.util.Random;
 public class GestionCartes {
 	public static <T> T extraire(List<T> liste) {
 		Random rand = new Random();
-	    int i = rand.nextInt(liste.size());
-	    T elt = liste.get(i);
-	    liste.remove(elt);
-	    return elt;
+		int i = rand.nextInt(liste.size());
+		T elt = liste.get(i);
+		liste.remove(elt);
+		return elt;
 	}
-	
+
 	public static <T> T extraireIt(List<T> liste) {
 		Random rand = new Random();
 		int indice = rand.nextInt(liste.size());
 		ListIterator<T> it = liste.listIterator();
 		T elt2 = liste.get(0);
-		for(int i = 0; i < indice; i++) {
+		for (int i = 0; i < indice; i++) {
 			elt2 = it.next();
 		}
 		it.remove();
 		return elt2;
 	}
-	
-	public <T> List<T> melanger(List<T> liste) {
-		for(int i = 0; i < liste.size(); i++) {
-			liste.remove(i);
+
+	public static <T> List<T> melanger(List<T> liste) {
+		int taille = liste.size();
+		List<T> liste2 = new ArrayList<T>();
+		for (int i = 0; i < taille; i++) {
+			liste2.add(extraire(liste));
 		}
-		return liste;
+		return liste2;
 	}
-	
-	public <T> boolean verifierMelange(List<T> liste1, List<T> liste2) {
-		for(int i = 0; i < liste1.size(); i++) {
-			if(Collections.frequency(liste1, liste1.get(i)) != Collections.frequency(liste2, liste2.get(i))) {
+
+	public static <T> boolean verifierMelange(List<T> liste1, List<T> liste2) {
+		for (T elt : liste1) {
+			if (Collections.frequency(liste1, elt) != Collections.frequency(liste2, elt)) {
+				return false;
+			}
+		}
+		for (T elt : liste2) {
+			if (Collections.frequency(liste2, elt) != Collections.frequency(liste1, elt)) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	public <T> List<T> rassembler(List<T> liste) {
-		List<T> liste2 = new ArrayList<T>();
-		for(ListIterator<T> it = liste.listIterator(); it.hasNext();) {
-			T elt = it.next();
-			for(int i = 0; i < liste2.size(); i++) {
-				if(liste2.get(i) == elt) {
-					liste2.add(i, elt);
+
+	public static <T> List<T> rassembler(List<T> liste) {
+		List<T> listeRassemble = new ArrayList<T>();
+		for (T elt : liste) {
+			int nbExemplaire = Collections.frequency(liste, elt);
+			if (!listeRassemble.contains(elt)) {
+				for (int i = 0; i < nbExemplaire; i++) {
+					listeRassemble.add(elt);
 				}
 			}
 		}
-		return liste2;
+		return listeRassemble;
 	}
-	
-	
-	public <T> boolean verifierRassemblement(List<T> liste) {
-		
-		for(ListIterator<T> it = liste.listIterator(); it.hasNext();) {
-			T elt = it.next();
-			
+
+	private static<T> boolean TrouverFinDeListe(List<T> liste, T elt, int i) {
+		boolean fin = false;
+		for (ListIterator<T> it = liste.listIterator(i); it.hasNext();) {
+			T elt2 = it.next();
+			if (elt.equals(elt2)) {
+				fin = true;
+			}
 		}
+		return fin;
 	}
+
+	public static <T> boolean verifierRassemblement(List<T> liste) {
+		T elemPrecedant = null;
+		for (ListIterator<T> it = liste.listIterator(); it.hasNext();) {
+			T elemSuivant = it.next();
+			if (elemPrecedant != null && !elemPrecedant.equals(elemSuivant)) {
+				if (TrouverFinDeListe(liste, elemSuivant, it.nextIndex())) {
+					return false;
+				}
+			}
+			elemPrecedant = elemSuivant;
+
+		}
+		return true;
+	}
+
 }
