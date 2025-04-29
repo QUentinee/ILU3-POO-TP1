c1ef76a5d13afd4c0195d8eb4cc3f3f8b03d3ad0
Quentin Heudé <que.heude@gmail.com>
2025-04-29 13:55:05 +0200

    finito le tp3 je crois

--- src/jeu/ZoneDeJeu.java
+++ src/jeu/ZoneDeJeu.java
@@ -1,5 +1,7 @@
 package jeu;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,12 +12,13 @@ import cartes.Carte;
 import cartes.DebutLimite;
 import cartes.FinLimite;
 import cartes.Limite;
+import cartes.Parade;
 import cartes.Type;
 
 public class ZoneDeJeu {
-    private LinkedList<Limite> pileLimites;
-    private LinkedList<Bataille> pileBataille;
-    private List<Borne> bornes;
+    private LinkedList<Limite> pileLimites = new LinkedList<>();
+    private LinkedList<Bataille> pileBataille = new LinkedList<>();
+    private Collection<Borne> bornes = new ArrayList<>();
     
     public int donnerLimitationVitesse() {
     	if(!pileLimites.isEmpty() && pileLimites.get(0) instanceof DebutLimite) {
@@ -33,7 +36,7 @@ public class ZoneDeJeu {
     	return somme;
     }
 
-	public List<Borne> getBornes() {
+	public Collection<Borne> getBornes() {
 		return bornes;
 	}
 	
@@ -47,7 +50,7 @@ public class ZoneDeJeu {
     
 	public void deposer(Carte c) {
 		if(c instanceof Borne b) {
-			bornes.add(0, b);
+			bornes.add(b);
 		}
 		else if(c instanceof Limite l) {
 			pileLimites.add(0, l);
@@ -61,13 +64,13 @@ public class ZoneDeJeu {
 		return pileBataille.isEmpty() || pileBataille.get(0).toString().equals("Feu vert");
 	}
 	
-	public boolean estDepotFeuVertAutorise() {
+	private boolean estDepotFeuVertAutorise() {
 		String contenu = pileBataille.get(0).toString();
 		
 		return pileBataille.isEmpty() || contenu.equals(Type.FEU.getAttaque()) || !contenu.equals(Type.FEU.getParade());
 	}
 	
-	public boolean estDepotBorneAutorise(Borne borne) {
+	private boolean estDepotBorneAutorise(Borne borne) {
 		int somme = 0;
 		for(Borne b : bornes) {
 			somme += b.getkm();
@@ -76,15 +79,50 @@ public class ZoneDeJeu {
 		return this.peutAvancer() && borne.getkm() <= 50 && somme < 1000;
 	}
 	
-	public boolean estDepotLimiteAutorise(Limite limite) {
+	private boolean estDepotLimiteAutorise(Limite limite) {
 		boolean a = limite instanceof DebutLimite && (pileLimites.get(0) instanceof FinLimite || pileLimites.isEmpty());
 		boolean b = limite instanceof FinLimite && pileLimites.get(0) instanceof DebutLimite;
 		return a || b;
 	}
 	
-	public boolean estDepotBatailleAutorise(Bataille bataille) {
+	private boolean estDepotBatailleAutorise(Bataille bataille) {
 		boolean attaque = bataille instanceof Attaque;
-		boolean a = bataille instanceof  
+	    boolean parade = bataille instanceof Parade;
+
+	    if (attaque) {
+	        return peutAvancer(); 
+	    }
+
+	    if (parade) {
+	        Parade p = (Parade) bataille;
+	        if (bataille.getType() == Type.FEU && bataille.toString().equals("Feu Vert")) {
+	            return pileBataille.isEmpty();
+//	                || pileBataille.peek() instanceof FeuRouge
+//	                || (pileBataille.peek() instanceof Parade && !(pileBataille.peek() instanceof FeuVert));
+	        } else {
+	            return !pileBataille.isEmpty()
+	                && pileBataille.peek() instanceof Attaque;
+//	                && pileBataille.peek().getClass().equals(p.getAttaqueCorrespondante());
+	        }
+	    }
+
+	    return false;
+	}
+	
+	public boolean estDepotAutorise(Carte carte) {
+		if(carte.toString().equals("Feu Vert")) {
+			return estDepotFeuVertAutorise();
+		}
+		else if(carte instanceof Borne borne) {
+			return estDepotBorneAutorise(borne);
+		}
+		else if(carte instanceof Limite limite) {
+			return estDepotLimiteAutorise(limite);
+		}
+		else {
+			Bataille bataille = (Bataille) carte;
+			return estDepotBatailleAutorise(bataille);
+		}
 	}
 	
 }
